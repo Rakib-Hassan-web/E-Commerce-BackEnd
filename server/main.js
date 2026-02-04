15a9c0a5465fec9efb853836ebac0c48f2720085
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-02-04 23:55:17 +0600

    connected

--- server/main.js
+++ server/main.js
@@ -13,5 +13,5 @@ app.listen(8000, () => {
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-  .then(() => console.log('Connected!'));
+mongoose.connect('mongodb+srv://E-commerceBackend:E-commerceBackend@cluster0.7ooynjm.mongodb.net/E-commerceBackend?appName=Cluster0')
+  .then(() => console.log(' mongo db Connected!'));
