6b6b7f93190ff9fc7a046676f0e8121624c9bd84
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-02-04 23:53:22 +0600

    mongoose and mongo db install

--- server/main.js
+++ server/main.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 
 
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000")
 })
+
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
