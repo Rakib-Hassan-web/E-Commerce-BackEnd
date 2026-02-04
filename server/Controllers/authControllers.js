92adc02381b01a8f8c6e97bb765895185c5a84bf
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-02-05 01:24:41 +0600

    validation

--- server/Controllers/authControllers.js
+++ server/Controllers/authControllers.js
@@ -1,15 +1,22 @@
+const { isValidEmail, isValidPassword } = require("../services/validation")
 
 
  const RegisterUSer = async (req ,res)=>{
   
     try {
        
- const {FullName ,Email ,Password } =req.body
+ const {fullName ,email ,password , phone } =req.body
 
-   if(!FullName ) return res.status(400).send({message : " FullName is Required"})
+   if(!fullName ) return res.status(400).send({message : " fullName is Required"})
+   if(!email ) return res.status(400).send({message : " email is Required"})
+    if(!isValidEmail(email)) return res.status(400).send({message : " Invalid Email"})
+   if(!password ) return res.status(400).send({message : " password is Required"})
+    if(!isValidPassword(password)) return res.status(400).send({message : " Invalid Password"})
+
+    res.status(201).send({message : "User Registered Successfully"})
         
     } catch (error) {
-        
+        res.status(500).send({message : "Internal Server Error"})
     }
  }
 
