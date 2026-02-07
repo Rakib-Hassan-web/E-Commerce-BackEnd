e33539ea3292bad6061caba05e62ec7db489db02
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-02-07 18:35:39 +0600

    login start

--- server/Controllers/authControllers.js
+++ server/Controllers/authControllers.js
@@ -1,22 +1,48 @@
 const userSchema = require("../models/userSchema")
-const { sendEmail } = require("../services/emailServices")
+const {
+  sendEmail
+} = require("../services/emailServices")
 const emailtemplate = require("../services/emailTemplate")
-const { generateOTP } = require("../services/helpers")
-const { isValidEmail, isValidPassword } = require("../services/validation")
+const {
+  generateOTP
+} = require("../services/helpers")
+const {
+  isValidEmail,
+  isValidPassword
+} = require("../services/validation")
 
-// -----------reg--------------
+// -----------registration--------------
 const RegisterUSer = async (req, res) => {
   try {
-    const { fullName, email, password, phone } = req.body;
+    const {
+      fullName,
+      email,
+      password,
+      phone
+    } = req.body;
 
-    if (!fullName) return res.status(400).send({ message: "fullName is Required" });
-    if (!email) return res.status(400).send({ message: "email is Required" });
-    if (!isValidEmail(email)) return res.status(400).send({ message: "Invalid Email" });
-    if (!password) return res.status(400).send({ message: "password is Required" });
-    if (!isValidPassword(password)) return res.status(400).send({ message: "Invalid Password" });
+    if (!fullName) return res.status(400).send({
+      message: "fullName is Required"
+    });
+    if (!email) return res.status(400).send({
+      message: "email is Required"
+    });
+    if (!isValidEmail(email)) return res.status(400).send({
+      message: "Invalid Email"
+    });
+    if (!password) return res.status(400).send({
+      message: "password is Required"
+    });
+    if (!isValidPassword(password)) return res.status(400).send({
+      message: "Invalid Password"
+    });
 
-    const existinguser = await userSchema.findOne({ email });
-    if (existinguser) return res.status(400).send({ message: "User Already Exists" });
+    const existinguser = await userSchema.findOne({
+      email
+    });
+    if (existinguser) return res.status(400).send({
+      message: "User Already Exists"
+    });
 
     const otp = generateOTP();
 
@@ -29,123 +55,137 @@ const RegisterUSer = async (req, res) => {
       otpExpires: new Date(Date.now() + 2 * 60 * 1000),
     });
 
-    await newUser.save(); 
+    await newUser.save();
 
     await sendEmail({
       email,
       subject: "Email Verification",
       otp,
       template: emailtemplate,
-     
+
     });
 
-    res.status(201).send({ message: "User Registered Successfully" });
+    res.status(201).send({
+      message: "User Registered Successfully"
+    });
 
   } catch (error) {
     console.error(error);
-    res.status(500).send({ message: "Internal Server Error" });
+    res.status(500).send({
+      message: "Internal Server Error"
+    });
   }
 };
 
 
 // -----------verifyOTP--------------
 
-const verifyOTP = async (req,res)=>{
- try {
-  const{ otp ,email}=req.body
+const verifyOTP = async (req, res) => {
+  try {
+    const {
+      otp,
+      email
+    } = req.body
 
-  if(!otp) return res.status(400).send({ message: "OTP is required"})
-  if(!email) return res.status(400).send({ message: "Email is required"})
+    if (!otp) return res.status(400).send({
+      message: "OTP is required"
+    })
+    if (!email) return res.status(400).send({
+      message: "Email is required"
+    })
 
     const user = await userSchema.findOne({
       email,
-       otp: Number(otp),
-      otpExpires: { $gt: new Date()},
-      isverified:false,
+      otp: Number(otp),
+      otpExpires: {
+        $gt: new Date()
+      },
+      isverified: false,
     })
 
-  if(!user) return res.status(400).send({ message: "Invalid or Expired OTP" })
+    if (!user) return res.status(400).send({
+      message: "Invalid or Expired OTP"
+    })
 
-     user.isverified=true
-     user.otp=null
-     user.otpExpires=null
-     await user.save()
-  
+    user.isverified = true
+    user.otp = null
+    user.otpExpires = null
+    await user.save()
 
-     res.status(200).send({ message: "OTP Verified Successfully" })
 
-  
- } catch (error) {
-  
-  res.status(500).send({ message: "Internal Server Error" });
- }
+    res.status(200).send({
+      message: "OTP Verified Successfully"
+    })
+
+
+  } catch (error) {
+
+    res.status(500).send({
+      message: "Internal Server Error"
+    });
+  }
 
 }
 
 
 // ------------resendOTP-------------
 
-const resendOTP = async (req , res)=>{
+const resendOTP = async (req, res) => {
   try {
-    // const { email } = req.body;
-    // if (!email) return res.status(400).send({ message: "Email is required" });
+    const {
+      email
+    } = req.body
 
-    // const user = await userSchema.findOne({ email });
-    // if (!user) return res.status(400).send({ message: "User not found" });
-
-    // const otp = generateOTP();
-
-    // user.otp = otp;
-    // user.otpExpires = new Date(Date.now() + 2 * 60 * 1000);
-    // await user.save();
-
-    // await sendEmail({
-    //   email,
-    //   subject: "Resend OTP",
-    //   otp,
-    //   template: emailtemplate,
-     
-    // });
-
-    // res.status(200).send({ message: "OTP Resent Successfully" });
+    if (!email) return res.status(400).send({
+      message: "Email is required"
+    });
 
 
-
-
-   const {email} = req.body
-
-if (!email) return res.status(400).send({ message: "Email is required" });
-
-
- const user = await userSchema.findOne({
+    const user = await userSchema.findOne({
       email,
-      isverified:false,
+      isverified: false,
     })
-  if(!user) return res.status(400).send({ message: "Invalid or Unverified User" })
+    if (!user) return res.status(400).send({
+      message: "Invalid or Unverified User"
+    })
 
     const otp = generateOTP();
-     user.otp= otp
-     user.otpExpires= new Date(Date.now() + 2 * 60 * 1000)
+    user.otp = otp
+    user.otpExpires = new Date(Date.now() + 2 * 60 * 1000)
 
-       await user.save(); 
+    await user.save();
 
     await sendEmail({
       email,
       subject: "Email Verification",
       otp,
       template: emailtemplate,
-     
+
     });
 
-    res.status(201).send({ message: "OTP Resent Successfully" });
+    res.status(201).send({
+      message: "OTP Resent Successfully"
+    });
 
 
 
   } catch (error) {
-  res.status(500).send({ message: "Internal Server Error" });
-    
+    res.status(500).send({
+      message: "Internal Server Error"
+    });
+
   }
 }
- 
 
- module.exports={RegisterUSer,verifyOTP,resendOTP}
+
+// ------------login------------
+
+
+
+
+
+module.exports = {
+  RegisterUSer,
+  verifyOTP,
+  resendOTP
+}
