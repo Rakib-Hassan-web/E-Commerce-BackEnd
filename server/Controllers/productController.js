db65141dc6319db9cf32fde583317236e87ea379
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-02-14 00:03:51 +0600

    product controller on process

--- server/Controllers/productController.js
+++ server/Controllers/productController.js
@@ -1,20 +1,29 @@
+const { uplodecloudinary } = require("../services/cloudinaryServices");
 
 const createNewProduct = async (req,res)=>{
     try {
         const {title,slug,description,category,price,discountPercentage,variants,tags,thumbnail,images} = req.body;
-
+         const thumbnail_IMG =req.files?.thumbnail
+         const Images_IMG =req.files?.images
         //  if (!title) return res.status(400).send({message: "title is Required" });
         //  if (!slug) return res.status(400).send({message: "slug is Required" });
         //  if (!description) return res.status(400).send({message: "description is Required" });
         //  if (!category) return res.status(400).send({message: "category is Required" });
-        //  if (!price) return res.status(400).send({message: "price is Required" });
+          //  if (!price) return res.status(400).send({message: "price is Required" });
         
-        //  if (!thumbnail) return res.status(400).send({message: "thumbnail is Required" });
+        //  if (!thumbnail_IMG || thumbnail_IMG.length === 0) return res.status(400).send({message: "Thumbnail is Required" });
         //  if (!images || !Array.isArray(images)) return res.status(400).send({message: "images is Required and must be an array" });
 
-        
+             let imagesUrl = [];
 
-        console.log(req.files); 
+        if (images) {
+            const resPromise = images.map(async (item) => uploadToCloudinary(item, "products"));
+            const results = await Promise.all(resPromise)
+            imagesUrl = results.map(r => r.secure_url)
+        }
+
+
+        // console.log(req.file); 
     } catch (error) {
   res.status(500).send({message: "Internal Server Error"})
     }
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,10 +1,14 @@
 const express =require('express')
 const { createNewProduct } = require('../Controllers/productController')
+const multer = require('multer')
+const authMiddleware = require('../middleware/authMiddleware')
+const { roleCheckMiddleware } = require('../middleware/roleCheckMiddleware')
 
 const routee =express.Router()
+const uplode = multer()
 
 
-routee.post("/create" ,createNewProduct)
+routee.post("/create" ,authMiddleware, roleCheckMiddleware('admin'),uplode.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'images', maxCount: 4 }]),createNewProduct)
 
 
 module.exports=routee
