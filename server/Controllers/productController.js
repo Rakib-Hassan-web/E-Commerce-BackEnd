25122c6f2427952d32bc78eefa9804e690044af1
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-02-15 23:46:51 +0600

    product controller all validation on process

--- server/Controllers/productController.js
+++ server/Controllers/productController.js
@@ -1,37 +1,45 @@
 const categorySchema = require("../models/categorySchema");
+const productSchema = require("../models/productSchema");
 const { uplodecloudinary } = require("../services/cloudinaryServices");
 const ENUM_SIZE = ["s", "m", "l", "xl", "2xl", "3xl"];
 
 const createNewProduct = async (req,res)=>{
     try {
-        const {title,slug,description,category,price,discountPercentage,variants,tags,thumbnail,images} = req.body;
-         const thumbnail_IMG =req.files?.thumbnail
-         const Images_IMG =req.files?.images
+        const {title,slug,description,category,price,discountPercentage,variants,tags} = req.body;
+         const thumbnail =req.files?.thumbnail
+         const images =req.files?.images
          if (!title) return res.status(400).send({message: "title is Required" });
          if (!slug) return res.status(400).send({message: "slug is Required" });
          if (!description) return res.status(400).send({message: "description is Required" });
          if (!category) return res.status(400).send({message: "category is Required" });
          const ExistingCategory = await categorySchema.findById(category);
-           if (!ExistingCategory) return res.status(400).send({message: "Invalid category" });
-           if (!price) return res.status(400).send({message: "price is Required" });
+         if (!ExistingCategory) return res.status(400).send({message: "Invalid category" });
+         if (!price) return res.status(400).send({message: "price is Required" });
         
-         if (!thumbnail_IMG || thumbnail_IMG.length === 0) return res.status(400).send({message: "Thumbnail is Required" });
-         if (!images || !Array.isArray(images)) return res.status(400).send({message: "images is Required and must be an array" });
 
 
-        //      let imagesUrl = [];
+         if (!thumbnail || thumbnail?.length === 0) return res.status(400).send({message: "Thumbnail is Required" });
+         if (images && images?.length > 4) return res.status(400).send({message: "You can upload images max 4" });
+         const thumnailUrl = await uplodecloudinary(thumbnail[0], "products")
 
-        // if (Images_IMG) {
-        //     const resPromise = Images_IMG.map(async (item) => uplodecloudinary(item, "products"));
-        //     const results = await Promise.all(resPromise)
-        //     imagesUrl = results.map(r => r.secure_url)
+        if (!thumnailUrl) return res.status(400).send({message: "Failed to upload thumbnail" });
 
-        //   }
-        //   console.log("imagesUrl" ,imagesUrl); 
+
+
+
+        let imagesUrl = [];
+
+        if (images) {
+            const resPromise = images.map(async (item) => uplodecloudinary(item, "products"));
+            const results = await Promise.all(resPromise)
+            imagesUrl = results.map(r => r.secure_url)
+
+          }
+      
 
 
     
-        if (!Array.isArray(variants) || variants.length === 0) return  res.status(400).send({message: "Minimum 1 variant is required." });
+       if (!Array.isArray(variants) || variants.length === 0) return  res.status(400).send({message: "Minimum 1 variant is required." });
         
       for (const element of variants) {
         
@@ -46,7 +54,21 @@ const createNewProduct = async (req,res)=>{
         if( new Set(ALL_Sku).size !== ALL_Sku.length) return res.status(400).send({message: "Duplicate SKU found."});
         console.log(ALL_Sku);
       }
-        
+
+
+      const newProduct = new productSchema({
+        title,
+        slug,
+        description,
+        category,price,
+        discountPercentage,
+        variants,
+        tags,
+        thumbnail: thumnailUrl.secure_url,
+        images: imagesUrl
+      })
+      await newProduct.save()
+      res.status(201).send({message: "Product created successfully", product: newProduct})
     } catch (error) {
       console.log(error);
       
