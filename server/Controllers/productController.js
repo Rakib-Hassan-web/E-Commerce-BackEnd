17d7e7c8db4e16d118599223522bd70a29146efc
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-02-15 23:29:20 +0600

    product controller all validation on process

--- server/Controllers/productController.js
+++ server/Controllers/productController.js
@@ -4,13 +4,13 @@ const ENUM_SIZE = ["s", "m", "l", "xl", "2xl", "3xl"];
 const createNewProduct = async (req,res)=>{
     try {
         const {title,slug,description,category,price,discountPercentage,variants,tags,thumbnail,images} = req.body;
-        //  const thumbnail_IMG =req.files?.thumbnail
-        //  const Images_IMG =req.files?.images
-        //  if (!title) return res.status(400).send({message: "title is Required" });
-        //  if (!slug) return res.status(400).send({message: "slug is Required" });
-        //  if (!description) return res.status(400).send({message: "description is Required" });
-        //  if (!category) return res.status(400).send({message: "category is Required" });
-          //  if (!price) return res.status(400).send({message: "price is Required" });
+         const thumbnail_IMG =req.files?.thumbnail
+         const Images_IMG =req.files?.images
+         if (!title) return res.status(400).send({message: "title is Required" });
+         if (!slug) return res.status(400).send({message: "slug is Required" });
+         if (!description) return res.status(400).send({message: "description is Required" });
+         if (!category) return res.status(400).send({message: "category is Required" });
+           if (!price) return res.status(400).send({message: "price is Required" });
         
         //  if (!thumbnail_IMG || thumbnail_IMG.length === 0) return res.status(400).send({message: "Thumbnail is Required" });
         //  if (!images || !Array.isArray(images)) return res.status(400).send({message: "images is Required and must be an array" });
