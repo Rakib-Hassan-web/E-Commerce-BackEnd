86caebe86b06b1e89510b9a75928926ca7a73e7c
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-03-11 23:35:02 +0600

    Add to cart  schema

---
+++ server/models/cartSchema.js
@@ -0,0 +1,45 @@
+ const mongoose =require("mongoose")
+
+
+const cartItems = new mongoose.Schema({
+    product:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"product",
+        required:true
+    },
+    sku:{
+    type:String,
+    required:true
+
+    },
+    Quantity:{
+        type:String,
+        required:true,
+        min:1,
+        default:1
+    },
+    subtotal:{
+          type:String,
+          required:true
+    }
+})
+
+
+
+ const CartSchema = new mongoose.Schema({
+    user:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User",
+      required:true,
+      unique:true
+    },
+    items:[],
+    totalproducts:{
+        type:Number,
+        default:0
+    },
+    totalprice:{
+       type:Number,
+        default:0 
+    }
+ } ,{timestamps:true})
