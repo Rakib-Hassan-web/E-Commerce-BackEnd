6b9309b2070743db109dce5dce059460b50d59e2
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-03-14 14:46:35 +0600

    add to cart controller st

--- server/Controllers/cartController.js
+++ server/Controllers/cartController.js
@@ -1,6 +1,12 @@
+const { sendError } = require("../services/responseHandler");
 
 const AddToCart = async (req,res)=>{
     try {
+
+        const {productId ,sku ,Quantity} = req.body;
+
+        if(!productId || !sku || !Quantity) sendError(res ,"Invalid Request" ,400)
+        if(!productId || !sku || !Quantity) sendError(res ,"Invalid Request" ,400)
         
     } catch (error) {
         
