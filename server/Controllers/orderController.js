2b4ffd3335e7961ed314152fc8a81dbd1b98aaa0
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-03-31 20:45:47 +0600

    cartid find

--- server/Controllers/orderController.js
+++ server/Controllers/orderController.js
@@ -1,6 +1,22 @@
+const cartSchema = require("../models/cartSchema")
+const { sendError } = require("../services/responseHandler")
+
 const checkOut = async(req ,res)=>{
   
-    res.send("ckk")
+
+    try {
+        
+       const {cartID} =req.body
+
+       if(!cartID) return sendError(res,"cartid is required" ,400)
+
+        const cartData = await cartSchema.findOne({_id:cartID})
+
+        console.log(cartData);
+        
+    } catch (error) {
+        sendError(res,"internal server error" ,500)
+    }
 }
 
 module.exports ={ checkOut }
